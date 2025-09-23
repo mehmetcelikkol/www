2770c49e33a7125b71fcdaa36f65eb28292c4b4c
mehmetcelikkol <mehmetcelikkol@hotmail.com>
2025-09-23 18:03:20 +0300

    Giteyukle denemesi (Sal-23.09.2025-_1803

---
+++ gite yukle.bat
@@ -0,0 +1,26 @@
+@echo off
+cd /d C:\wamp64\www
+
+echo -------------------------------------
+echo  Git otomatik commit & push basliyor
+echo -------------------------------------
+
+:: Kullanıcıdan commit mesajı al
+set /p usermsg=Commit mesaji giriniz: 
+
+:: Tarih + saat ekle
+for /f "tokens=1-3 delims=/ " %%a in ("%date%") do (
+    for /f "tokens=1-2 delims=: " %%x in ("%time%") do (
+        set msg=%usermsg% (%%a-%%b-%%c_%%x%%y)
+    )
+)
+
+git add .
+git commit -m "%msg%"
+git push origin master
+
+echo -------------------------------------
+echo  Commit ve push tamamlandi!
+echo -------------------------------------
+
+pause
