709093a98f9fdace5eae4b7df6f51c880b44bd37
mehmetcelikkol <mehmetcelikkol@hotmail.com>
2025-09-30 01:54:28 +0300

    enerji analizi - temalar düzeltildi (Sal-30.09.2025-_ 154

--- gite yukle.bat
+++ gite yukle.bat
@@ -10,14 +10,15 @@ set /p usermsg=Commit mesaji giriniz:
 
 :: Tarih + saat ekle
 for /f "tokens=1-3 delims=/ " %%a in ("%date%") do (
-    for /f "tokens=1-2 delims=: " %%x in ("%time%") do (
+    for /f "tokens=1-2 delims=:." %%x in ("%time%") do (
         set msg=%usermsg% (%%a-%%b-%%c_%%x%%y)
     )
 )
 
-git add .
-git commit -m "%msg%"
-git push origin master
+:: Git komutlarını tam yol ile çalıştır
+"C:\Program Files\Git\cmd\git.exe" add .
+"C:\Program Files\Git\cmd\git.exe" commit -m "%msg%"
+"C:\Program Files\Git\cmd\git.exe" push origin master
 
 echo -------------------------------------
 echo  Commit ve push tamamlandi!
